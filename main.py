388928159e1960ee183443608dfcd87fd5392641
SanderKarbus <sander.karbus@vikk.ee>
2026-04-10 11:45:41 +0300

    Remove HTTPBearer reference and fix typo

--- main.py
+++ main.py
@@ -221,8 +221,6 @@ app = FastAPI(
 )
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
-app.security = HTTPBearer(auto_error=False)
-
 
 @app.get("/")
 async def root():
